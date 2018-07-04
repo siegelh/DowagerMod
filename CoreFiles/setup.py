c8146e24026158fc83b715454960e6786e787201
Harrison <harrisried@gmail.com>
2018-07-03 22:12:23 -0700

    updated installer, added Install.exe

--- CoreFiles/setup.py
+++ CoreFiles/setup.py
@@ -9,3 +9,14 @@ from distutils.core import setup
 import py2exe
 
 setup(console=['install.py'])
+
+"""
+import sys
+from cx_Freeze import setup, Executable
+
+setup(
+    name = "DowagerMod",
+    version = "0.2.1",
+    description = "You know golly well what this is.",
+    executables = [Executable("install.py", base = "Win32GUI")])
+	"""
