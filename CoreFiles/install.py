889ffc4183575962a0797f604102380ebb8cccd2
Harrison <harrisried@gmail.com>
2018-07-03 20:26:55 -0700

    updated install script

--- CoreFiles/install.py
+++ CoreFiles/install.py
@@ -9,15 +9,53 @@ import sys
 import distutils.dir_util as dis
 import winsound
 
-version = '0.1.0'
+version = '0.2.1'
 print('Preparing to install HarrisonMod version %s, please wait...' % version)
 winsound.PlaySound('install_noise.wav',winsound.SND_FILENAME)
 
-src_path = sys.argv[0][:-16].replace('/','\\') + 'assets'
-civ_path = input('Type the path to where Civ Beyond the Sword is installed (ex: C:\Program Files (x86)\Steam\SteamApps\common\): ').replace("\n",'\\') + "\\Sid Meier's Civilization IV Beyond the Sword\\Beyond the Sword\\Assets"
+## Functions
+def findPath(civ_drive):
+"""
+Returns either the path where Civ 4 is installed or 'Failed'.
+"""
+	for root, directories, filenames in os.walk(civ_path.upper() + ':\\Program Files (x86)')
+		for directory in directories:
+			print(os.path.join(root, directory))
+			current_path = os.path.join(root, directory).replace("\n",'\\')
+			if current_path.contains("\\Sid Meier's Civilization IV Beyond the Sword\\Beyond the Sword\\Assets"):
+				print("""
+				---------------------------------
+				Civ 4 BTS Assets Directory found!
+				---------------------------------
+				
+				{0}
+				
+				""".format(current_path)
+				response = input("Is this the correct directory? [y / n]: ")
+				response = response.lower()
+				if response == "y":
+					return current_path
+				else:
+					pass
+	return "Failed"
+								
+def installMod(civ_directory):
+	dis.copy_tree(src_path,civ_path)
+	print('Finished installing HarrisonMod version %s!' % version)
 
-dis.copy_tree(src_path,civ_path)
-print('Finished installing HarrisonMod version %s!' % version)
-exit = input('Click enter to exit setup.')
+## Fetch install path
+src_path = sys.argv[0][:-16].replace('/','\\') + 'assets'
+civ_drive = input('Type the letter drive name (caps insensitive) where Civ Beyond the Sword is installed: ')
+civ_path = findPath(civ_drive)
+if civ_path == "Failed":
+	print("Could not find Assets directory. Installation failed. Goodbye!")
+else:
+	print("""
+	
+	Installing DowagerMod to {0}
+	
+	""".current_path)
+	installMod(civ_path)
+	exit = input('Success! Click enter to exit setup.')
               
            
